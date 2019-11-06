93e9e3b2902a435ecc38b76ecefa319dc5a87cf2
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 15:55:01 +0800

    1

---
+++ src/M2Q18.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class M2Q18 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int sum=0,d=0;
+        String str="";
+    }
+}
